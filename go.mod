ca213d9e423b2a13ccf46c021c3ba1db5f545f44
Daniel <Daniel.Ding@dell.com>
2020-08-04 17:47:20 +0800

    introduce assert

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module int-fun
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -1,6 +1,10 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestBreadthFirstSearch(t *testing.T) {
 	graph := [][]int{
@@ -11,6 +15,8 @@ func TestBreadthFirstSearch(t *testing.T) {
 		{0, 1, 1, 1, 0, 0},
 		{0, 0, 1, 0, 0, 0},
 	}
+
+	expectQ := []int{0, 1, 3, 4, 2, 5}
 	queue := BreadthFirstSearch(graph, 0)
-	t.Log(queue)
+	assert.Equal(t, queue, expectQ, "Wrong")
 }
